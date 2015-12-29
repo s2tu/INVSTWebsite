c9b3cddcb8f9dfdc525f5501b25cdfbe76c06d70
Sonny <iamsonnytu@hotmail.com>
2015-12-29 15:32:40 -0500

    updated front page and about us page

--- js/intro.js
+++ js/intro.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$(".main").fadeIn();
 	$(".image_animation").addClass("slide");
-	$(".text1").addClass("txtcss");
-	$(".text2").addClass("txtcss3");
+	//$(".text1").addClass("txtcss");
+	//$(".text2").addClass("txtcss3");
 	$(".navibar").addClass("txtcss2");
 }); 
 
